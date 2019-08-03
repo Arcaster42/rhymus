363cadcdfb2b5dade895bbceef6dbe4d7cd34162
TJ Epperson <tj.epperson070@gmail.com>
2019-08-03 20:56:43 +0900

    Added spec file for game_logic

---
+++ tests/webspec.js
@@ -0,0 +1,30 @@
+
+
+describe('Game Logic Tests', () => {
+    it('should eventually have tests', () => {
+        expect(1).to.equal(1)
+    })
+    it('should contain a RhymusGame class', () => {
+        expect(typeof RhymusGame).to.equal('function')
+        const Game = new RhymusGame()
+        expect(typeof Game).to.equal('object')
+    })
+    it('should have an assignListeners function', () => {
+        expect(typeof assignListeners).to.equal('function')
+    })
+    it('should have an updateDisplay function', () => {
+        expect(typeof updateDisplay).to.equal('function')
+    })
+    it('should have a checkAnswer function', () => {
+        expect(typeof checkAnswer).to.equal('function')
+    })
+    it('should have a correctAnswer function', () => {
+        expect(typeof correctAnswer).to.equal('function')
+    })
+    it('should have an incorrectAnswer function', () => {
+        expect(typeof incorrectAnswer).to.equal('function')
+    })
+    it('should have a gameOver function', () => {
+        expect(typeof gameOver).to.equal('function')
+    })
+})

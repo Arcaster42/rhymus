2928b449baed6ed5afe287a2c5a88320ec1707f4
TJ Epperson <tj.epperson070@gmail.com>
2019-08-04 13:22:22 +0900

    Added additional element tests

--- tests/webspec.js
+++ tests/webspec.js
@@ -1,9 +1,4 @@
-
-
 describe('Game Logic Tests', () => {
-    it('should eventually have tests', () => {
-        expect(1).to.equal(1)
-    })
     it('should contain a RhymusGame class', () => {
         expect(typeof RhymusGame).to.equal('function')
         const Game = new RhymusGame()
@@ -26,5 +21,11 @@ describe('Game Logic Tests', () => {
     })
     it('should have a gameOver function', () => {
         expect(typeof gameOver).to.equal('function')
+    })
+    it('should have necessary elements set as constants', () => {
+        expect(cardBlock).to.not.be.undefined
+        expect(countElement).to.not.be.undefined
+        expect(rhymeElement).to.not.be.undefined
+        expect(guessElement).to.not.be.undefined
     })
 })

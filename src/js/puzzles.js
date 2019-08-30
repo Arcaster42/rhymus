b3b907cb22096ac2c9a1e608359c29903d965013
dhuang612 <dhuang@madisoncollege.edu>
2019-08-29 21:54:06 -0400

    added in three more sentences

--- src/js/puzzles.js
+++ src/js/puzzles.js
@@ -1,39 +1,60 @@
 var puzzlesArray = [
-    {
-        id: 1,
-        sentence: 'I have to walk my log.',
-        word: 'log',
-        hint: 'animal',
-        answer: 'dog'
-    },
-    {
-        id: 2,
-        sentence: 'I did not hear the phone because I was a sheep.',
-        word: 'a sheep',
-        hint: 'tired',
-        answer: 'asleep'
-    },
-    {
-        id: 3,
-        sentence: 'I will go grocery stopping tomorrow.',
-        word: 'stopping',
-        hint: 'chore',
-        answer: 'shopping',
-    },
-    {
-        id: 4,
-        sentence: 'I like to play with my set.',
-        word: 'set',
-        hint: 'home animal',
-        answer: 'pet',
-    },
-    {
-        id: 5, 
-        sentence: 'My favorite drink is poke.',
-        word: 'poke',
-        hint: 'red and white label',
-        answer: 'coke',
-    },
-]
+  {
+    id: 1,
+    sentence: 'I have to walk my log.',
+    word: 'log',
+    hint: 'animal',
+    answer: 'dog'
+  },
+  {
+    id: 2,
+    sentence: 'I did not hear the phone because I was a sheep.',
+    word: 'a sheep',
+    hint: 'tired',
+    answer: 'asleep'
+  },
+  {
+    id: 3,
+    sentence: 'I will go grocery stopping tomorrow.',
+    word: 'stopping',
+    hint: 'chore',
+    answer: 'shopping'
+  },
+  {
+    id: 4,
+    sentence: 'I like to play with my set.',
+    word: 'set',
+    hint: 'home animal',
+    answer: 'pet'
+  },
+  {
+    id: 5,
+    sentence: 'My favorite drink is poke.',
+    word: 'poke',
+    hint: 'red and white label',
+    answer: 'coke'
+  },
+  {
+    id: 6,
+    sentence: 'I am on a float',
+    word: 'float',
+    hint: 'water',
+    answer: 'boat'
+  },
+  {
+    id: 7,
+    sentence: 'Here is my new hat',
+    word: 'hat',
+    hint: 'house pet',
+    answer: 'cat'
+  },
+  {
+    id: 8,
+    sentence: 'I won the space',
+    word: 'space',
+    hint: 'competition',
+    answer: 'race'
+  }
+];
 
-module.exports = puzzlesArray
+module.exports = puzzlesArray;

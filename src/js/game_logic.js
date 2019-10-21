4d3757c310121b27362ca632c9ed8a1d3bc3de0d
Carlos Medina <carlos.w.med@gmail.com>
2019-10-20 22:16:58 -0400

    refactor: hiding hint bg and displaying it when hint shows

--- src/js/game_logic.js
+++ src/js/game_logic.js
@@ -126,6 +126,7 @@ const incorrectAnswer = () => {
     wrongElement.textContent = 'incorrect guesses: ' + incorrectGuesses
     //if there are more than 4 incorrect guesses on a single card the player loses
     if (incorrectGuesses > 2 && incorrectGuesses < 4) {
+        hint.style.display = 'flex';
         hint.textContent = 'Hint: ' + capitalizeFirstLetter()
     } else {
         gameOver()

236a1827ada627b176a19d3fa34f16c4aa18d12e
Yurie <cutecupcakeandcute@gmail.com>
2020-02-21 00:54:44 +0900

    add Header file to create Header component (start/restart btn)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Header />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
